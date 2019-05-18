22e3da2c213e0187cac383b053db0db48db1917e
iincho <yy.motoyama@gmail.com>
2019-05-18 16:14:44 +0900

    Initial Commit

---
+++ SweTranslationNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweTranslationNote
+//
+//  Created by 本山洋一 on 2019/05/18.
+//  Copyright © 2019 本山洋一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SweTranslationNoteTests/SweTranslationNoteTests.swift
@@ -0,0 +1,34 @@
+//
+//  SweTranslationNoteTests.swift
+//  SweTranslationNoteTests
+//
+//  Created by 本山洋一 on 2019/05/18.
+//  Copyright © 2019 本山洋一. All rights reserved.
+//
+
+import XCTest
+@testable import SweTranslationNote
+
+class SweTranslationNoteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
